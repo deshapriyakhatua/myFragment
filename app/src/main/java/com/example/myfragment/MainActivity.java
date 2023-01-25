ca2d49735c65e95226e72529be89717b0de7a3e2
deshapriya <deshapriyakhatua@gmail.com>
2023-01-25 17:37:39 +0530

    first commit

---
+++ app/src/main/java/com/example/myfragment/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1,button2;
+    FrameLayout fragmentContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        fragmentContainer = findViewById(R.id.fragmentContainer);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeFragmentToContainer(new Fragment1());
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeFragmentToContainer(new Fragment2());
+            }
+        });
+
+    }
+
+    private void placeFragmentToContainer(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
